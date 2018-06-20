491ee1a9cad72ca0c60b51f2443c396485af53a7
yang <yang-pin@live.com>
2018-06-20 19:26:56 +0200

    yes

--- Desktop/robocode/robotssrc/nl/saxion/dhi1vsq4/Leader.java
+++ Desktop/robocode/robotssrc/nl/saxion/dhi1vsq4/Leader.java
@@ -1,6 +1,7 @@
 package nl.saxion.dhi1vsq4;
 
 import robocode.*;
+import robocode.util.Utils;
 
 import java.awt.geom.Point2D;
 import java.io.IOException;
@@ -31,7 +32,6 @@ public class Leader extends TeamRobot {
             }catch(Exception ignored){}
         }
             setAdjustGunForRobotTurn(true);
-        //noinspection InfiniteLoopStatement
         while (true) {
             scan();
             setAhead(300);
@@ -48,16 +48,16 @@ public class Leader extends TeamRobot {
         }
     }
 
-    public void bulletShielding(ScannedRobotEvent e) {
-        double changeInEnergy = previousEnergy - e.getEnergy();
-        if (changeInEnergy > 0 && changeInEnergy <= 3) {
-            setTurnGunRight(getHeading() - getGunHeading() + e.getBearing());
-            setFire(0.1);
-        }
-    }
+//    public void bulletShielding(ScannedRobotEvent e) {
+//        double changeInEnergy = previousEnergy - e.getEnergy();
+//        if (changeInEnergy > 0 && changeInEnergy <= 3) {
+//            setTurnGunRight(getHeading() - getGunHeading() + e.getBearing());
+//            setFire(0.1);
+//        }
+//    }
 
     public void scan() {
-        turnRadarRight(360);
+        turnRadarRight(Double.POSITIVE_INFINITY);
         try {
             sendLocation(lowestRobotLocation);
         } catch (RuntimeException e) {
@@ -87,20 +87,27 @@ public class Leader extends TeamRobot {
 
     @Override
     public void onScannedRobot(ScannedRobotEvent e) {
-        bulletShielding(e);
-
+//        bulletShielding(e);
+//        if (e.getEnergy() <= lowest) {
+//            this.lowest = e.getEnergy();
+//
+//            double angleToEnemy = e.getBearing();
+//
+//            // Calculate the angle to the scanned robot
+//            double angle = Math.toRadians((robotStatus.getHeading() + angleToEnemy % 360));
+//
+//        }
         if(isTeammate(e.getName())){
-         System.out.println("isTeam");}
+            System.out.println("isTeam");}
         else {
-            if (e.getEnergy() <= lowest) {
-                this.lowest = e.getEnergy();
-                double angleToEnemy = e.getBearing();
+            double radarTurn =
+                    // Absolute bearing to target
+                    getHeadingRadians() + e.getBearingRadians()
+                            // Subtract current radar heading to get turn required
+                            - getRadarHeadingRadians();
 
-                // Calculate the angle to the scanned robot
-                double angle = Math.toRadians((robotStatus.getHeading() + angleToEnemy % 360));
-                setFire(Math.min(400 / e.getDistance(), 3));
-            }
-
+            setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
+            setFire(Math.min(400 / e.getDistance(), 3));
         }
     }
 
